4d4c312f047ae8ce9bcfa752c81f584274336145
Dawid <dharendarz2@gmail.com>
2020-12-24 18:43:26 +0100

    Added condition which changes hours.

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
-const showTime = () => {
-    const newDate = new Date();
+let showTime = () => {
+    let newDate = new Date();
     let hour = newDate.getHours();
     let minutes = newDate.getMinutes();
     let seconds = newDate.getSeconds();
-    let time = `${hour}:${minutes}:${seconds}`
+    let period = "AM";
+
+    if(hour == 0 ){
+        hour = 12;
+    }else if(hour >= 12){
+        hour = hour - 12;
+        period = "PM";
+    }
+    hour = hour < 10 ? "0" + hour : hour; 
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds
+
+    const time = `${hour}:${minutes}:${seconds}:${period}`;
     setInterval(showTime,1000)
-    const timer = document.querySelector(".clock").innerText = time
+    document.querySelector(".clock").innerText = time
 }
 const showDate = () => {
     const date = new Date();
     const year = date.getFullYear();
-    const month = date.getMonth();
-    const day = date.getDay();
-    const dayNumeric = date.getDate()
+    const month = date.getMonth()+1;
+    const dayNumeric = date.getDate();
+    
     const today = `${dayNumeric}.${month}.${year}`
-    const data = document.querySelector(".date").innerText = today
-
+    document.querySelector(".date").innerText = today
 }
 showTime();
 showDate();
