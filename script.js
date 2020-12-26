23a79efe46ee5faa8cf9231b610c2de965416d21
Dawid <dharendarz2@gmail.com>
2020-12-26 15:56:05 +0100

    Added function which gets user location like latitude and longitude.

--- script.js
+++ script.js
@@ -17,16 +17,22 @@ let showTime = () => {
 
     const time = `${hour}:${minutes}:${seconds}:${period}`;
     setInterval(showTime,1000)
-    document.querySelector(".clock").innerText = time
+    document.querySelector(".Date__Clock").innerText = time
 }
 const showDate = () => {
     const date = new Date();
     const year = date.getFullYear();
     const month = date.getMonth()+1;
     const dayNumeric = date.getDate();
-    
     const today = `${dayNumeric}.${month}.${year}`
-    document.querySelector(".date").innerText = today
+    document.querySelector(".Date").innerText = today
 }
+const getLocation = () => {
+        navigator.geolocation.getCurrentPosition(position => {
+        let latitude = position.coords.latitude;
+        let longtiude = position.coords.longitude;
+    })
+}
+
 showTime();
 showDate();
