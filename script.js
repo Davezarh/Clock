f97ca674d22c2668790913629989a6d737add0b2
Dawid <dharendarz2@gmail.com>
2021-01-03 10:49:28 +0100

    Added condition to getLocation, if your permission is denied, user see message on the screen.

--- script.js
+++ script.js
@@ -28,9 +28,16 @@ const showDate = () => {
     document.querySelector(".Date").innerText = today
 }
 const getLocation = () => {
-    document.querySelector(".Date__button").addEventListener ("click", () => { navigator.geolocation.getCurrentPosition(position => {
-    let latitude = position.coords.latitude;
-    let longtiude = position.coords.longitude;
+    document.querySelector(".Date__button").addEventListener ("click", () => { navigator.geolocation.getCurrentPosition(succesCallBack,errorCallBack)
+    function errorCallBack(error){
+        if(error.code == error.PERMISSION_DENIED){
+            document.querySelector(".Date__Location").innerText = ("Nie pozwoliłeś na pobranie twojej lokalizacji 😢")
+        }
+    }
+
+    function succesCallBack(position){
+     let latitude = position.coords.latitude;
+     let longtiude = position.coords.longitude;
         fetch(`https://eu1.locationiq.com/v1/reverse.php?key=pk.5d92a33884eeadee31093ffee72e312b&lat=${latitude}&lon=${longtiude}&format=json`)
             .then(response => {
                 if(!response.ok){
@@ -47,16 +54,15 @@ const getLocation = () => {
                     Ulica: ${userAddres.city_district || userAddres.neighbourhood }
                     Kraj: ${userAddres.country} 
                     `)},
-                    console.log(data),
                 )}
             )
-            .catch(error => {                                                                     
+            .catch(error => {                                                                  
                 console.error(error)
                 document.querySelector(".Date__button").addEventListener("click", () => {                                                        
                 document.querySelector(".Date__Location").innerText = (`Oops... Coś poszło nie tak \ud83e\udd2f Sprawdź, czy masz połaczenie z internetem`)})
                 })
-    })}) 
-}
+    }})} 
+
 getLocation() 
 showTime();
 showDate();
